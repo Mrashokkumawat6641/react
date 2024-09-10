89065b131f4d7163f2a03fdfb55cd1a32cade8a8
Mr-Ashok <mrashokkumawat6641@gmail.com>
2024-09-10 19:59:13 +0530

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import React from './react'
+
+function App() {
+  return (
+   <React/>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/react.jsx
@@ -0,0 +1,7 @@
+function React() {
+    return ( 
+        <h3>React or code</h3>
+    )
+}
+
+export default React
