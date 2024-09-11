d86f186e16ff09ba9882b46d03bc9f451ec5e0a6
Mr-Ashok <mrashokkumawat6641@gmail.com>
2024-09-11 17:45:11 +0530

    update the code

--- 01basiccreat/src/App.js
+++ 01basiccreat/src/App.js
@@ -6,8 +6,31 @@ function App() {
     <>
     <React/>
     <h1>my first react project</h1>
+    
+
+    </>
+  )
+} 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXovdoSs.jpg',
+  imageSize: 90,
+};
+export default function Profile() {
+  return (
+    <>
+    <h1>{user.name}</h1>
+    <img
+    className='avator'
+    src='{user.imageUrl'}
+    alt={'photo of ' + user.name}
+    style= {{
+      width:user.imageSize,
+      height: user.imageSize
+    }}
     </>
   )
 }
+
 
 export default App;
